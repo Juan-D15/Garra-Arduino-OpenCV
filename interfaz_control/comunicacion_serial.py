009cff8a63fdf8154e5e63f436e73638f99b61de
Juan Díaz <jchendiaz15@gmail.com>
2025-03-08 15:57:50 -0600

    Archivos para la comunicación serial

---
+++ interfaz_control/comunicacion_serial.py
@@ -0,0 +1,9 @@
+import serial
+
+class ComunicacionSerial:
+    def __init__(self):
+        self.com = serial.Serial("COM3", 9600, write_timeout=10) #Puerto del Arduino (COMX), frecuencia de comunicación serial(9600)
+    
+    def sending_data(self, command: str) -> None:
+        self.com.write (command.encode('ascii'))
+        print(f'DATA: {command}')
