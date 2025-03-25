b9acce7a80b304127c6b5a5d3131523389bb6f42
Juan Díaz <jchendiaz15@gmail.com>
2025-03-24 22:18:28 -0600

    configuracion timeout

--- interfaz_control/comunicacion_serial.py
+++ interfaz_control/comunicacion_serial.py
@@ -2,7 +2,7 @@ import serial
 
 class ComunicacionSerial:
     def __init__(self):
-        self.com = serial.Serial("COM3", 9600, write_timeout=10) #Puerto del Arduino (COMX), frecuencia de comunicación serial(9600)
+        self.com = serial.Serial("COM3", 9600, write_timeout=15) #Puerto del Arduino (COMX), frecuencia de comunicación serial(9600)
     
     def sending_data(self, command: str) -> None:
         self.com.write (command.encode('ascii'))
